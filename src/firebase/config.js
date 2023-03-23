43fd69fb4366e2c9819ed047f8859a7d1f2b9aef
Ayush Singh <ayushsops@gmail.com>
2023-03-23 18:21:16 +0530

    Intiailizing firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcalJKsLFikP5MnjN4HtTee8ST3QY5U8w",
+  authDomain: "money-master-83e11.firebaseapp.com",
+  projectId: "money-master-83e11",
+  storageBucket: "money-master-83e11.appspot.com",
+  messagingSenderId: "392998533208",
+  appId: "1:392998533208:web:f902ea5c19ca4f008b322d"
+};
+
+// Intializing Firebase
