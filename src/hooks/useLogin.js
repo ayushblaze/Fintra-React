4855afd72c1a076bdfd3d73cadb0fdaeafe39081
Ayush Singh <ayushsops@gmail.com>
2023-03-28 16:48:14 +0530

    Conditionally showing info, using the login hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,34 +2,25 @@ import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const login = async (email, password) => {
     setError(null);
     setIsPending(true);
 
+    //sign the user out
     try {
-      // signup user
-      const res = await projectAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-      console.log(res.user);
+      console.log("Email from useLogin:", email);
+      const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
-      if (!res) {
-        throw new Error("Could not complete signup");
-      }
-
-      // add display name to user
-      await res.user.updateProfile({ displayName });
-
-      // dispatch the login action
+      // dispatch logout action
       dispatch({ type: "LOGIN", payload: res.user });
 
+      // update state
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
@@ -47,5 +38,5 @@ export const useSignup = () => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { signup, error, isPending };
+  return { login, error, isPending };
 };
