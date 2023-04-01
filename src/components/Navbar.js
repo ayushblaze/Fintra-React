eed1ba8dcd52ad533329896bff852ff36559a3c0
Ayush Singh <ayushsops@gmail.com>
2023-04-01 21:23:43 +0530

    Stepback - 1 Day

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
-
-// styles
-import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { logout } = useLogout();
@@ -12,17 +10,16 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}>Fintra</li>
         {!user && (
           <>
             <li>
               <Link to="/login">Login</Link>
-            </li>
-            <li>
               <Link to="/signup">Signup</Link>
             </li>
           </>
         )}
+
         {user && (
           <>
             <li>hello, {user.displayName}</li>
